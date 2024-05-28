6e0ae589b592bd988190347a5cce80a861b34e63
JonathanDDavis <125523206+JonathanDDavis@users.noreply.github.com>
2024-05-28 14:54:51 -0500

    Add files via upload

---
+++ bookReport/js/navbar.js
@@ -0,0 +1,87 @@
+var navbar = '<ul>\
+				<li>\
+					<a href="#">Home</a>\
+				</li>\
+				<li>\
+					<a>Chapters</a>\
+						<ul>\
+							<li>\
+							<a href="#">Chapter1</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter2</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter3</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter4</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter5</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter6</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter7</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter8</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter9</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter10</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter11</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter12</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter13</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter14</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter15</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter16</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter17</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter18</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter19</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter20</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter21</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter22</a>\
+							</li>\
+                            <li>\
+							<a href="#">Chapter23</a>\
+							</li>\
+                            
+						</ul>\
+				</li>\
+				<li>\
+				<a href="#">About</a>\
+				</li>\
+			</ul>'
+	
+function loadNav() {
+	document.getElementById('nav').innerHTML = navbar;	
+}
